684bab33c18b43e9d6a73d93d529d0ae24f16461
ruimendes29 <ruifilipe29mendes@gmail.com>
2021-04-11 00:25:58 +0100

    started vue project

---
+++ roomie-project/src/components/FillInfo.vue
@@ -0,0 +1,36 @@
+<template>
+  <form>
+    <div class="campo">
+      <div class="input_name">
+        <label class="p">{{ definicao }}</label>
+      </div>
+      <input class="l" :type=tipo required />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ['definicao', 'tipo'],
+};
+</script>
+
+<style>
+.campo {
+  text-align: center;
+  font-size: 2vw;
+}
+.input_name {
+  text-align: left;
+  margin-left: 4%;
+}
+.p {
+  display: block;
+}
+.l {
+  border-radius: 0%;
+  width: 90%;
+  height: 0%;
+  font-size:100%;
+}
+</style>
---
+++ roomie-project/src/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="grid-container">
+      <h1>Welcome!</h1>
+      <FillInfo definicao="Username" :tipo="'text'"/>
+      <FillInfo definicao="Password" :tipo="'password'"/>
+      <button class="button">Login</button>
+    </div>
+</template>
+
+<script>
+import FillInfo from './FillInfo.vue'
+export default {
+  components: {FillInfo},
+  data(){
+    return {
+    }
+  }
+};
+</script>
+
+<style>
+body{
+  background-color: #dfdfdf;
+}
+.grid-container {
+  background-color: rgb(204, 204, 204);
+  border-radius: 2%;
+  display: grid;
+  margin: 15vh auto;
+  width: 60%;
+  height: 60vh;
+  min-height: 400px;
+  row-gap: 10%;
+  align-content: center;
+  grid-template-columns: auto;
+  text-align:center;
+}
+.button{
+  text-align: center;
+  width: 30%;
+  padding: 0%;
+  font-size: 150%;
+  margin: 0 auto;
+}
+
+</style>
