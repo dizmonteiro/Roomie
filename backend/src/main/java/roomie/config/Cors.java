46d0034f5df7f51e3d6042b57021a223c16c1b37
Vasco Ramos <vascoalramos@outlook.pt>
2021-04-11 19:38:33 +0100

    Add CORS support

---
+++ backend/src/main/java/roomie/config/Cors.java
@@ -0,0 +1,24 @@
+package roomie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: Vasco Ramos
+ * @created: 11/04/2021 - 18:57
+ */
+
+@Configuration
+public class Cors {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("DELETE", "POST", "PUT", "GET");
+			}
+		};
+	}
+}
