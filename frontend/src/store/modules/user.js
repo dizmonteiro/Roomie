211a04bdc69b4f89702a39bb3c8a1b17472f0da6
Diogo Ribeiro <ribeiropdiogo@gmail.com>
2021-04-19 10:51:11 +0100

    User Type Redirect

--- frontend/src/store/modules/user.js
+++ frontend/src/store/modules/user.js
@@ -1,27 +1,29 @@
 import { USER_REQUEST, USER_ERROR, USER_SUCCESS } from "../actions/user";
 import Vue from "vue";
 import { AUTH_LOGOUT } from "../actions/auth";
-import axios from 'axios'
 
-const state = { status: "", profile: {} };
+const state = { status: "", email: "", type: "", name: "" };
 
 const getters = {
   getProfile: state => state.profile,
+  getType: state => state.profile.type,
   isProfileLoaded: state => !!state.profile.name
 };
 
 const actions = {
   [USER_REQUEST]: ({ commit, dispatch }) => {
     commit(USER_REQUEST);
-    axios({ url: "/" })
-      .then(resp => {
-        commit(USER_SUCCESS, resp);
-      })
-      .catch(() => {
-        commit(USER_ERROR);
-        // if resp is unauthorized, logout, to
-        dispatch(AUTH_LOGOUT);
-      });
+
+    var resp = {}
+    resp.type = "landlord"
+    resp.name = "Madje"
+    resp.email = "a@a.pt"
+    
+    commit(USER_SUCCESS, resp);
+
+    if(resp.type == "qqcoisa"){
+      dispatch(AUTH_LOGOUT)
+    }
   }
 };
 
@@ -31,7 +33,9 @@ const mutations = {
   },
   [USER_SUCCESS]: (state, resp) => {
     state.status = "success";
-    Vue.set(state, "profile", resp);
+    Vue.set(state, "type", resp.type);
+    Vue.set(state, "email", resp.email);
+    Vue.set(state, "name", resp.name);
   },
   [USER_ERROR]: state => {
     state.status = "error";
