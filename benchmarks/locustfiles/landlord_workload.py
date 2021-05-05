da6c490046d161f1461299a6843661d00e2d2745
Vasco Ramos <vascoalramos@outlook.pt>
2021-05-05 10:30:23 +0100

    Init tenant workload

--- benchmarks/locustfiles/landlord_workload.py
+++ benchmarks/locustfiles/landlord_workload.py
@@ -12,15 +12,60 @@ from config import UserSimulationConfig
 class LandlordWorkload(HttpUser):
     wait_time = UserSimulationConfig.wait_time_between_tasks
 
+    user = None
+    token = None
+    houses = []
+
     def on_start(self):
-        self.client.get("https://github.com/")
+        self.register()
+        self.login()
+        print(self.user)
+        print(self.token)
 
     @task
     def simulate(self):
         pass
 
-    @task
-    def index(self):
+    def register(self):
+        payload = {
+            "email": "ld@test5.com",
+            "password": "test",
+            "username": "ld",
+            "name": "Vasco Ramos",
+            "birthDate": "2021-04-14",
+            "sex": "male",
+            "nif": "111111111",
+            "address": "Av 1 B1 2E, Viseu",
+            "phone": "9111111111",
+        }
+        response = self.client.post(
+            "/api/landlords", data=payload, files={"foo": "bar"}
+        )
+        self.user = payload
+        self.user["id"] = response.json()["id"]
+
+    def login(self):
+        headers = {"content-type": "application/json"}
+        payload = {"email": "ld@test5.com", "password": "test"}
+        response = self.client.post("/api/auth/login", json=payload, headers=headers)
+        self.token = response.json()["token"]
+
+    def post_house(self):
         headers = {"content-type": "application/json"}
         payload = {"email": "test@test.com", "password": "test"}
-        response = self.client.post("/api/auth/login", json=payload, headers=headers)
+        self.client.post("/api/auth/login", json=payload, headers=headers)
+
+    def get_houses(self):
+        headers = {"content-type": "application/json"}
+        payload = {"email": "test@test.com", "password": "test"}
+        self.client.post("/api/auth/login", json=payload, headers=headers)
+
+    def get_applications(self):
+        headers = {"content-type": "application/json"}
+        payload = {"email": "test@test.com", "password": "test"}
+        self.client.post("/api/auth/login", json=payload, headers=headers)
+
+    def get_tenant_profile(self):
+        headers = {"content-type": "application/json"}
+        payload = {"email": "test@test.com", "password": "test"}
+        self.client.post("/api/auth/login", json=payload, headers=headers)
