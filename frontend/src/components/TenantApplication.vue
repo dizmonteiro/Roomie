6702a1ed305dd519e96cb94b81decb65a6a157ce
ruimendes29 <ruifilipe29mendes@gmail.com>
2021-06-08 17:07:33 +0100

    added remove application feature

--- frontend/src/components/TenantApplication.vue
+++ frontend/src/components/TenantApplication.vue
@@ -3,7 +3,12 @@
     <div class="box sCard">
       <div class="columns is-desktop">
         <div
-          class="column adjust-hero-s has-text-centered is-one-fifth-desktop is-full-mobile is-full-tablet"
+          class="
+            column
+            adjust-hero-s
+            has-text-centered
+            is-one-fifth-desktop is-full-mobile is-full-tablet
+          "
         >
           <div class="box sBox">
             <agile class="sCarousel" :dots="false" :slidesToShow="1">
@@ -28,7 +33,13 @@
           </div>
         </div>
         <div
-          class="column adjust-hero-s is-four-fifths-desktop is-full-mobile is-full-tablet form has-text-centered"
+          class="
+            column
+            adjust-hero-s
+            is-four-fifths-desktop is-full-mobile is-full-tablet
+            form
+            has-text-centered
+          "
         >
           <div class="middle">
             <h1 class="title is-2 status">Status: {{ application.status }}</h1>
@@ -38,15 +49,39 @@
             >
               Check House
             </a>
+            <a class="button is-green ap" @click="openModal">
+              Remove Application
+            </a>
           </div>
         </div>
       </div>
+    </div>
+    <div
+      :id="'remove' + application.tenantId + '_' + application.id"
+      class="modal"
+    >
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <div class="box has-text-centered">
+          <h1 class="title">Are you sure you want to remove?</h1>
+          <button class="button d is-green" @click="cancel">Confirm</button>
+          <button class="button d is-green" @click="closeModal">Close</button>
+        </div>
+      </div>
+      <button
+        class="modal-close is-large"
+        aria-label="close"
+        value="close-modal"
+        @click="closeModal"
+      ></button>
     </div>
   </div>
 </template>
 
 <script>
 import { VueAgile } from "vue-agile";
+import axios from "axios";
+import { url as api_url } from "@/assets/scripts/api";
 export default {
   props: ["application"],
   components: { agile: VueAgile },
@@ -54,9 +89,26 @@ export default {
     return {};
   },
   methods: {
+    openModal() {
+      var s = "remove" + this.application.tenantId + "_" + this.application.id;
+      document.getElementById(s).classList.add("is-active");
+    },
+    closeModal() {
+      var s = "remove" + this.application.tenantId + "_" + this.application.id;
+      document.getElementById(s).classList.remove("is-active");
+    },
     cancel() {
       console.log(this);
-      this.$emit("cancel", this);
+      axios
+        .delete(api_url + "/api/applications/" + this.application.id)
+        .then((response) => {
+          var s =
+            "remove" + this.application.tenantId + "_" + this.application.id;
+          document.getElementById(s).classList.remove("is-active");
+          console.log(response);
+          location.reload();
+        })
+        .catch((e) => console.log(e));
     },
   },
 };
@@ -73,8 +125,12 @@ export default {
   border-color: #2d6a4f;
   border-style: solid;
 }
+.d{
+  margin: 0 3%;
+}
 .ap {
-  width: 50%;
+  width: 40%;
+  margin: 0 1%;
 }
 .buttons {
   margin: 4% auto 0 auto;
